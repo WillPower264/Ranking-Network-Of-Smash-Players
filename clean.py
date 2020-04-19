f7f815cb71f25ac253676de6fac1f3bae328bf67
WilliamLi264264 <william.li@princeton.edu>
2020-04-18 23:16:55 -0400

    also record the tournaments and attendance

--- clean.py
+++ clean.py
@@ -1,8 +1,10 @@
 import csv
 
 desired_fields = ['winner_name', 'winner_global_id', 'loser_name', 'loser_global_id', 'winner_score', 'loser_score']
+extra_fields = ['tournament_name']
 field_indices = {}
 players = {}
+tournaments = {}
 
 # check if entry has valid data
 def isValid(line):
@@ -32,6 +34,20 @@ def savePlayers(line):
         players[loser_id] = [loser_name]
     elif loser_name not in players[loser_id]:
         players[loser_id].append(loser_name)
+
+# keep track of the tournaments and number of attendees
+def saveTournament(line):
+    tournament_name = line[field_indices['tournament_name']]
+    if tournament_name not in tournaments:
+        tournaments[tournament_name] = 0
+    tournaments[tournament_name] += 1
+
+# combine information for desired fields
+def getInfo(line):
+    obj = {}
+    for x in desired_fields:
+        obj[x] = line[field_indices[x]]
+    return obj
     
 # read the data from file
 with open('ultimate_sets.csv') as sets_data:
@@ -42,19 +58,19 @@ with open('ultimate_sets.csv') as sets_data:
         for i, header in enumerate(headers):
             if header in desired_fields:
                 field_indices[header] = i
+            if header in extra_fields:
+                field_indices[header] = i
 
         # write header to file
         writer = csv.DictWriter(cleaned, fieldnames=desired_fields)
         writer.writeheader()
 
-        obj = {}
         # process each line
         for line in data_reader:
             if not isValid(line): continue
             savePlayers(line)
-            for x in desired_fields:
-                obj[x] = line[field_indices[x]]
-            writer.writerow(obj)
+            saveTournament(line)
+            writer.writerow(getInfo(line))
 
 # write player and id information
 with open('ultimate_player_ids.csv', mode='w') as player_ids:
@@ -62,6 +78,13 @@ with open('ultimate_player_ids.csv', mode='w') as player_ids:
     writer = csv.DictWriter(player_ids, fieldnames=['id', 'player'])
     writer.writeheader()
     for player_id in sorted(players.keys()):
-        obj = {'id': player_id, 'player': ' [or] '.join(players[player_id])}
-        writer.writerow(obj)
+        writer.writerow({'id': player_id, 'player': ' [or] '.join(players[player_id])})
+
+# write tournament name and attendance
+with open('ultimate_tournament_attendance.csv', mode='w') as tounrament_attendance:
+    # write header to file
+    writer = csv.DictWriter(tounrament_attendance, fieldnames=['tournament', 'count'])
+    writer.writeheader()
+    for tournament in sorted(tournaments.keys()):
+        writer.writerow({'tournament': tournament, 'count': tournaments[tournament]})
 
